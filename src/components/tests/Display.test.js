721783d83f600ce8ba5cd306e0386d9972a22fb8
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-05-23 18:38:29 -0500

    third testing suite comeplete

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -52,3 +52,16 @@ test('renders show season options matching your data when the button is clicked'
     expect(seasonOptions).toHaveLength(2)
 });
 })
+
+test('displayFunc if called wehen the fetch button is pressed', async () => { 
+    mockFetchShow.mockResolvedValueOnce(testShow)
+    const displayFunc = jest.fn()
+    render(<Display displayFunc={displayFunc}/> )
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled()
+    })
+
+})
